5cb388da3ab0ea32323e838bff3877556988f4af
Alejandro1998elebebecito <49255376+Alejandro1998elebebecito@users.noreply.github.com>
2019-09-04 14:04:21 -0500

    Add Quick Sort

--- quick.h
+++ quick.h
@@ -5,9 +5,40 @@
 
 class QuickSort : public Sort {
     public:
-        QuickSort(int *elements, size_t size, int l, int e ) : Sort(elements, size) { l=l; e=e;}
+        QuickSort(int *elements, size_t size) : Sort(elements, size) { }
+
+    void quickSort(int elements[], int l, int h)
+    {
+        if (l < h)
+        {
+            int p = partition(elements, l, h);
+            quickSort(elements, l, p - 1);
+            quickSort(elements, p + 1, h);
+        }
+    }
+    int partition(int elements[], int l, int h)
+    {
+        int i=l-1;
+        int pivot = elements[h];
+        int temp;
+        for (int j = l;j < h;j++)
+        {
+            if (elements[j] <= pivot)
+            {
+                i++;
+                temp = elements[j];
+                elements[j] = elements[i];
+                elements[i] = temp;
+            }
+        }
+        elements[h] = elements[i + 1];
+        elements[i + 1] = pivot;
+        return (i + 1);
+    }
         void execute() override
         {
+            quickSort(elements, 0, size-1);
+
         }
 
         inline string name() override { return "QuickSort"; }
--- sort.h
+++ sort.h
@@ -9,12 +9,8 @@ class Sort {
     protected:
         int *elements;
         size_t size;
-        int l, e;
     public:
     Sort(int *elements, size_t size) : elements(elements), size(size) {}
-    Sort(int *elements, size_t size, int l, int  e) : elements(elements), size(size), l{0}  {this->e = size-1;}  ;
-
-
 
     virtual void execute() = 0;
 
